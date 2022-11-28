2fbdf9e924068864720ded18a626e07f0355e8de
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2022-11-28 01:17:49 -0500

    bg stuff

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(360, 240, PlayState));
+		addChild(new FlxGame(300, 170, PlayState));
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,6 +3,9 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.math.FlxMath;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 
 class PlayState extends FlxState
 {
@@ -15,9 +18,13 @@ class PlayState extends FlxState
 	{
 		FlxG.sound.playMusic("assets/music/kissykiss.ogg", 1);
 
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, 0xFFcc99bb);
+		var bg:FlxSprite = new FlxSprite().loadGraphic("assets/images/bg" + FlxG.random.int(1, 3) + ".png", true, 300, 177);
+		bg.animation.add("idle", [0, 1, 2], 6);
+		bg.animation.play("idle");
 		bg.scrollFactor.set();
 		add(bg);
+
+		FlxTween.tween(bg, {y: -7}, 1.6, {ease: FlxEase.circInOut, type: PINGPONG});
 
 		personL = new Kisser(LEFT);
 		personR = new Kisser(RIGHT);
@@ -27,6 +34,8 @@ class PlayState extends FlxState
 
 		kissing = new Kissing();
 		add(kissing);
+
+		FlxG.camera.followLerp = 0.02;
 
 		super.create();
 	}
@@ -40,9 +49,21 @@ class PlayState extends FlxState
 
 		if (btnKissP)
 		{
-			kissing.screenCenter();
+			kissing.screenCenter(X);
 			kissing.y = personL.y;
 		}
+
+		if (btnKiss)
+		{
+			FlxG.camera.scroll.y = kissing.y + 20;
+		}
+		else
+		{
+			// FlxG.camera.follow(null);
+			FlxG.camera.scroll.set(0, 0);
+		}
+
+		FlxG.camera.zoom = FlxMath.lerp(FlxG.camera.zoom, btnKiss ? 1.3 : 1, 0.1);
 
 		personL.visible = personR.visible = !btnKiss;
 		kissing.visible = btnKiss;
